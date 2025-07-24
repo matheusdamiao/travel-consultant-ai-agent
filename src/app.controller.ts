f17c7215ffff9c83d03cdad1cecfc5d534f1f5cd
Matheus Damião <matheus.damiaoliveira@gmail.com>
2025-07-23 22:01:08 -0300

    log: body of webhook msg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -39,7 +39,7 @@ export class AppController {
 @HttpCode(200) // Importante: retornar 200 para Huggy saber que recebeu com sucesso
 async handleWebhook(@Body() body: any, @Headers() headers: any): Promise<string> {
   console.log('Webhook recebido!', body);
-  console.log('Headers:', headers);
+  console.log('dados reais:', body.messsages.receivedMessage?.[0]);
 
   // Extract message from webhook body
   const getMessageFromWebhook = (body: any): string | undefined => {
